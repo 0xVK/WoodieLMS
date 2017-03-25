eda18e6ded243e76c1c7668a128835fa7c46a3ff
kucher <koocherov@ya.ru>
2017-03-25 10:39:16 +0200

    updated

--- ALearning/settings.py
+++ ALearning/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'ALearning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'woodie_db',
+        'USER': 'admin',
+        'PASSWORD': 'indb9best',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
